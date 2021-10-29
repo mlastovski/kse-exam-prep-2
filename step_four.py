a0048588168ad1fdc61d232b18c43952336d7fdc
mlastovski <mail.emloft@gmail.com>
2021-10-30 01:55:57 +0300

    update

--- step_four.py
+++ step_four.py
@@ -4,12 +4,14 @@ import sys
 n = int(sys.argv[1])
 # print(n)
 m = int(sys.argv[2])
-
+z = m
 secret = random.randint(1, n)
 # print(secret)
-
+a = 1
+b = n
 while True:
-    guess = int(input('your guess: '))
+    guess = random.randint(a, b)
+    print("\n||| " + str(guess) + " |||\n")
 
     if guess != secret:
         m -= 1
@@ -21,15 +23,16 @@ while True:
 
         if guess < secret:
             print('secret is more than ' + str(guess) + '\n')
+            a = guess + 1
         if guess > secret:
             print('secret is less than ' + str(guess)+ '\n')
-    
+            b = guess - 1
     if m == 0:
         print('\nYou lost.')
         print("Secret was " + str(secret))
         exit()
     
     if guess == secret:
-        print("\nWOW WOW you won!!!")
+        print("\nWOW WOW you won in " + str(z-m+1) + ' moves!')
         exit()
 
